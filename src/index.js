94a757c6b8bb31465e630bf8dad7b54ca90eb18b
Zach Parks <zach@alliware.com>
2024-01-11 18:34:42 -0600

    More IDE setup and ignore files.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * This is a print function for testing.
+ * @param {string} message A message to print to the console.
+ * @returns {void}
+ */
+export function print(message) {
+    // eslint-disable-next-line no-console
+    console.log(message);
+}
